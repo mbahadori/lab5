8d66b3843545134d96b96026ee0e73fe23875e37
Evie <erkim@potomaschool.org>
2021-10-07 09:26:59 -0400

    Adding the test File

--- GitTestEvie.py
+++ GitTestEvie.py
@@ -1,4 +1,4 @@
 # Hey this is a new file made by Evie
 # following Mr. Gillespie's demonstration of how Git works
 
-# commiting = saving to local repository
+# committing = saving to local repository
